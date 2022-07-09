41a259d14b853d363dad172f7ceda5e2bb8b5f55
Zach Davis <zacharydavis92@gmail.com>
2022-07-09 14:28:05 -0500

    'added offline presistance'

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -6,6 +6,7 @@ import { QUERY_PRODUCTS } from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
 import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
+import { idbPromise } from '../../utils/helpers';
 
 function ProductList() {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
@@ -15,13 +16,34 @@ function ProductList() {
   const { currentCategory } = state;
 
   useEffect(() => {
+    // if there is data to be stored
     if (data) {
+      // let's store it in the global state object
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products
       });
+
+      // but let's also take each product and save it to IndexedDB using the helper function
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+
+      // add else if to check if 'loading' is undefined in 'useQuery()' Hook
+      // if we're offline and we run the 'useQuery()' hook, we'll never be in a state of loading data.
+      // this means that the 'loading' response Apollo provides to indicate that we're still waiting for a response won't exist
+      // because we're no longer waiting for data. The data simply isn't coming.
+    } else if (!loading) {
+      // since we're offline, get all of the data from the 'products' store
+      idbPromise('products', 'get').then((products) => {
+        // use retrieved data to set global state for offline browsing
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products
+        });
+      });
     }
-  }, [data, dispatch]);
+  }, [data, loading, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
